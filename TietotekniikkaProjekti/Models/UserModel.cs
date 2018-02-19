081f83d495453585fc2a64713aea754da234d79b
Tapio Riihimäki <s302994@OPI-A2063-D18>
2018-02-19 10:32:45 +0200

    Add project files.

---
+++ TietotekniikkaProjekti/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TietotekniikkaProjekti.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Nimi { get; set; }
+        public string Osoite { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string EmployeeType { get; set; }
+    }
+}
