b65b2ff8494b1d121e28c2bef16af905b5ef4dd3
Tapio Riihimäki <s302994@OPI-A2063-D18>
2018-02-19 12:29:25 +0200

    juu

--- TietotekniikkaProjekti/Controllers/HomeController.cs
+++ TietotekniikkaProjekti/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TietotekniikkaProjekti.Models;
+using System.DirectoryServices;
 
 namespace TietotekniikkaProjekti.Controllers
 {
@@ -36,6 +37,36 @@ namespace TietotekniikkaProjekti.Controllers
         public IActionResult AdminCreate()
         {
             return View();
+        }
+        public IActionResult Login()
+        {
+           
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel login)
+        {
+            Authenticate(login.Login, login.Password, "domaini");
+            return View();
+        }
+        public static void Rename(string server,string userName, string password, string objectDn, string newName)
+        {
+            DirectoryEntry child = new DirectoryEntry("LDAP://" + server + "/" +
+                objectDn, userName, password);
+            child.Rename("CN=" + newName);
+        }
+        private bool Authenticate(string userName, string password, string domain)
+        {
+            bool authentic = false;
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry("LDAP://" + domain,
+                    userName, password);
+                object nativeObject = entry.NativeObject;
+                authentic = true;
+            }
+            catch (DirectoryServicesCOMException) { }
+            return authentic;
         }
     }
 }
