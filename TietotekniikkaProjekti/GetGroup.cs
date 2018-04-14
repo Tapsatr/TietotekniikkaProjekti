13d873de3d08bd094ad1ce534b70b17763c19e49
Tapio Riihimäki <s302994@OPI-A2063-D18>
2018-04-14 13:22:40 +0300

    check if admin

---
+++ TietotekniikkaProjekti/GetGroup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TietotekniikkaProjekti
+{
+    public class GetGroup
+    {
+        private const string LDAP_PATH = "CN=Users,DC=ryhma1,DC=local";
+
+
+        public bool isAdmin (string username)
+        {
+
+            string stringDomainName = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            // set up domain context
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, stringDomainName, LDAP_PATH);
+
+            // find a user
+            UserPrincipal user = UserPrincipal.FindByIdentity(ctx, username);
+
+            // find the group in question
+            GroupPrincipal group = GroupPrincipal.FindByIdentity(ctx, "Administrators");
+
+            if (user != null)
+            {
+                // check if user is member of that group
+                if (user.IsMemberOf(group))
+                {
+                    return true;
+                    // do something.....
+                }
+            }
+            return false;
+        }
+    }
+}
